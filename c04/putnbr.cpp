87a5d411347db9bcc849a08f173bbe4ede1642b3
gianfrizio <vittoriociampi83@gmail.com>
2025-05-20 19:23:40 +0200

    Exercise update

--- c04/putnbr.cpp
+++ c04/putnbr.cpp
@@ -11,6 +11,11 @@ void ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
+    if (nb == -2147483648)
+    {
+        write (1, "-2147483648", 11);
+        return;
+    }
     if (nb < 0) 
     {                  
         ft_putchar('-');
